86252d36b7d54243c86277dea79728eb0d5b5838
Hai Nguyen <hainm@users.noreply.github.com>
2016-06-29 13:36:51 -0400

    Update source_phenix_2376.sh

--- phenix/source_phenix_2376.sh
+++ phenix/source_phenix_2376.sh
@@ -1,8 +1,15 @@
 #!/bin/sh
 
 py_anaconda=/home/haichit/anaconda3/envs/py2/lib/python2.7/site-packages/
+unset PYTHONPATH
 source /home/haichit/programs/phenix-installer-dev-2376-intel-linux-2.6-x86_64-centos5/phenix_env.sh
 source /home/haichit/source_ambertools_15.sh
 export PYTHONPATH=$PYTHONPATH:$py_anaconda
+export LD_LIBRARY_PATH=/home/haichit/anaconda3/envs/py2/lib/:$LD_LIBRARY_PATH
 alias phenix.ipython="phenix.python -c 'from IPython import start_ipython;start_ipython()'"
+alias phenix.jupyter="phenix.python -m jupyter"
+alias phenix.mdsrv="phenix.python -m mdsrv"
+alias phenix.nglview="phenix.python -m nglview"
+source activate py2
+
 
