7be36a0a1f4e3038b2f9af815fa19b0dfa066b49
Hai Nguyen <hainm@users.noreply.github.com>
2016-08-10 12:51:45 -0400

    Update source_phenix_2376.sh

--- phenix/source_phenix_2376.sh
+++ phenix/source_phenix_2376.sh
@@ -1,18 +1,13 @@
 #!/bin/sh
 
-py_anaconda=/home/haichit/anaconda3/envs/py2/lib/python2.7/site-packages/
 unset PYTHONPATH
 source /home/haichit/programs/phenix-installer-dev-2376-intel-linux-2.6-x86_64-centos5/phenix_env.sh
-source /home/haichit/source_ambertools_15.sh
-export PYTHONPATH=$PYTHONPATH:$py_anaconda
-export LD_LIBRARY_PATH=/home/haichit/anaconda3/envs/py2/lib/:$LD_LIBRARY_PATH
+source /home/haichit/source_amber_git.sh
+
+export PYTHONPATH=$PYTHONPATH:$AMBERHOME/miniconda/lib/python2.7/site-packages/
+
 alias phenix.ipython="phenix.python -c 'from IPython import start_ipython;start_ipython()'"
 alias phenix.jupyter="phenix.python -m jupyter"
 alias phenix.mdsrv="phenix.python -m mdsrv"
 alias phenix.nglview="phenix.python -m nglview"
-
-# you should install any extra packages in py_anaconda
-# conda install (after activating py2 env)
 source activate py2
-
-
